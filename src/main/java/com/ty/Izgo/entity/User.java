c58063eee99725efbc2b71c75b774d36f1ba9451
Sagar  Gaikwad <sagar.gaikwad.0018@gmail.com>
2023-11-10 15:26:33 +0530

    address ,booking enities are added

--- src/main/java/com/ty/Izgo/entity/User.java
+++ src/main/java/com/ty/Izgo/entity/User.java
@@ -1,5 +1,26 @@
 package com.ty.Izgo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	@Column(nullable = false)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+	private long phone;
+	private String licenseNo;
+	@Column(nullable = false)
+	private String role;
 }
